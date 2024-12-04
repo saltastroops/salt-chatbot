c54d9a2a2eb1618ca50cea005c4b7faa1f25ae2d
mofokeng-chaka <mofokeng.chk@gmail.com>
2024-12-04 08:27:23 +0200

    use pydantic

--- chatbot_api/models/response.py
+++ chatbot_api/models/response.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 
 class Response(BaseModel):
     """Final response to the question being asked"""
